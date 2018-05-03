a64679e412c3d726ca8871dbe7a59a335256232c
fabiana11 <fnfabianabe@gmail.com>
2018-05-02 21:45:15 -0300

    O Novo

---
+++ ei.php
@@ -0,0 +1,79 @@
+<html lang="en-US" prefix="og: http://ogp.me/ns#"><head>
+<meta charset="UTF-8">
+<title>Mih Store a Flat Ecommerce Bootstrap Responsive Web Template by w3layouts</title>
+
+
+<link rel="stylesheet" href="https://w3layouts.com/wp-content/themes/w3layouts/css/style-web.css?18Nov2016">
+<link href="//fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Oxygen:400,700,300" rel="stylesheet" type="text/css">
+<link href="https://w3layouts.com/wp-content/themes/w3layouts/fonts/icons.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://w3layouts.com/wp-content/themes/w3layouts/js/web/flexy-menu.js"></script>
+<script type="text/javascript" src="https://w3layouts.com/wp-content/themes/w3layouts/js/web/move-top.js"></script>
+<script type="text/javascript" src="https://w3layouts.com/wp-content/themes/w3layouts/js/web/easing.js"></script>
+<script type="text/javascript" src="https://w3layouts.com/wp-content/themes/w3layouts/js/jquery.magnific-popup.js"></script>
+<meta name="google-site-verification" content="pmhrR3CsQPEpuw32hx8i9G3Tm45rV0rb8psQ2Y-kG6Y">
+
+
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">      
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	
+
+
+<body>
+<?php 
+include 'index.php';
+ ?>
+<div class="mfp-bg my-mfp-zoom-in mfp-ready">
+	
+</div>
+<div class="mfp-wrap mfp-close-btn-in mfp-auto-cursor my-mfp-zoom-in mfp-ready" tabindex="-1" style="top: 0px; position: absolute; height: 576px;"><div class="mfp-container mfp-inline-holder">
+	<div class="mfp-content">
+		<div id="small-dialog" class="">
+								<div class="modal-content modal-info">
+									<div class="modal-header">
+										<h3>Login</h3>
+									</div>
+									<div class="modal-body modal-spa">
+										<div class="login-form">
+											<form action="autetincacao.php" method="post">
+												<input type="text" class="user" name="loginid" placeholder="Email" required="">
+												<input type="password" class="lock" name="Userpassword" placeholder="password" required="" value="">
+												<input type="hidden" name="sendbacktopage" value="/mih-store-a-flat-ecommerce-bootstrap-responsive-web-template/?_ga=2.203227605.574189616.1525304541-907112354.1525304541">
+												<div class="signin-rit">
+													<span class="checkbox1">
+														 <label class="checkbox"><input type="checkbox" name="checkbox" checked="">Remember me</label>
+													</span>
+													<a class="forgot play-icon popup-with-zoom-anim" href="#small-dialog3">Forgot Password?</a>
+													<div class="clear"> </div>
+												</div>
+												<input type="submit" value="Login">
+											</form>
+											<p>Novo?<a href="#small-dialog2" class="play-icon popup-with-zoom-anim">Create Account</a></p>
+										</div>
+
+
+									</div>
+								</div>
+						<a href="index.php" title="Close (Esc)" type="button" class="mfp-close">×</a></div></div></div></div>
+
+
+
+	
+	    
+
+
+
+
+
+
+
+
+
+
+
